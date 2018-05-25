035e3ef2a89d36426113c20b32a6e773fcf04e3e
Carrie Savinell <carrielane89@gmail.com>
2018-05-25 21:38:40 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -63,7 +63,7 @@ class Owner
     end   
     @pets.clear
   end  
-  binding.pry
+ # binding.pry
   def list_pets
       "I have #{pets[:fishes].length} fish, #{pets[:dogs].length} dog(s), and #{pets[:cats].length} cat(s)."
   end   
