278208e07087c3a6bc0a6dd72b9260689c93ff67
Carrie Savinell <carrielane89@gmail.com>
2018-05-25 19:49:29 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,33 @@
+require 'pry'
+
 class Owner
-  # code goes here
+
+  attr_accessor :pets 
+  attr_reader :species 
+  
+  @@counter = 0 
+  @@all = []
+  
+  def self.all 
+    @@all << self 
+  end   
+  
+  def self.counter
+    @@counter += 1 
+  end 
+  
+  def self.reset 
+    self.clear 
+  end   
+ #   binding.pry 
+  
+  
+  def initialize(owner)
+    @owner = owner
+    @@all << owner
+  end   
+    
+
+
 end
+
